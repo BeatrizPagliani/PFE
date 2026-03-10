43ccfed330d1f167815936b779f2303535124453
Beatriz Pagliani <beatrizsales.pcc@gmail.com>
2026-03-10 00:51:39 +0000

    Projeto Next Profile

---
+++ profile-site/components/Profile.js
@@ -0,0 +1,18 @@
+export default function Profile() {
+  return (
+    <div style={{textAlign: "center", marginTop: "40px"}}>
+      <img
+        src="yourphoto.jpeg"
+        alt="Minha foto"
+        width="200"
+        style={{borderRadius: "50%"}}
+      />
+
+      <h2>Beatriz Sales</h2>
+
+      <p>
+       Sou estudante de Sistemas para Internet, tenho 18 anos, gosto muito de ler e de formula 1.
+      </p>
+    </div>
+  );
+}
