bfe1cbb049d78151cc4ceadc02dbb0e9fb2b64de
Beatriz Pagliani <beatrizsales.pcc@gmail.com>
2026-04-07 22:06:56 +0000

    .

--- jogo-dado/components/Dado.js
+++ jogo-dado/components/Dado.js
@@ -1,18 +1,3 @@
-export default function Dado({ valor }) {
-  const imagens = {
-    1: "/dados/dado1.png",
-    2: "/dados/dado2.png",
-    3: "/dados/dado3.png",
-    4: "/dados/dado4.png",
-    5: "/dados/dado5.png",
-    6: "/dados/dado6.png",
-  };
-
-  return (
-    <img
-      src={imagens[valor]}  
-      alt={`Dado ${valor}`}
-      width={70}
-    />
-  );
+export default function Dado() {
+  return <img src="/dados/dado5.png" width={70} />;
 }
